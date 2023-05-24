256a6237fa47f9747bb0116d2d80589a59985fba
tsuzuki-seita <continew2002@gmail.com>
2023-05-24 14:12:11 +0900

    プレイヤーを動かせるようにした
    スクリプトPlayerManagerを作成し、矢印キーまたはAWSDで操作できるようにした。見えないオブジェクトを置いて地面以外には動けないようにした。

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    // PlayerのAnimator
+    //public Animator playerAnimator;
+    // PlayerのRigidbody
+    public Rigidbody playerRigidbody;
+
+    // 以下変数
+    // 移動速度の速さを指定
+    public float maxSpeed = 9f;
+    // PlayerSpriteの初期サイズを保存する変数
+    Vector3 defaultLocalScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 初期状態でPlayerの大きさを保存
+        defaultLocalScale = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 移動の横方向をInputから値で取得
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        //現在のy軸の位置を取得
+        float posY = transform.position.y;
+        //現在のX軸の位置を取得
+        float posX = transform.position.x;
+
+        // 速度をセットする
+        playerRigidbody.velocity = new Vector2(horizontalInput * maxSpeed, verticalInput * maxSpeed);
+
+        // もし左右のキーどちらかが押されているなら
+        if (horizontalInput != 0)
+        {
+            // キャラがどっちに向いているかを判定する
+            float direction = Mathf.Sign(horizontalInput);
+            // キャラの向きをキーの押された方向に指定する
+            transform.localScale = new Vector3(defaultLocalScale.x * direction, defaultLocalScale.y, defaultLocalScale.z);
+        }
+    }
+}
